a946921facfee297009afa4e1889d6d8973fe336
Jack Sorrels <sorrelsjack@gmail.com>
2021-10-24 18:27:54 -0400

    More anon access

--- MouthfeelAPIv2/MouthfeelAPIv2/Controllers/CommentsController.cs
+++ MouthfeelAPIv2/MouthfeelAPIv2/Controllers/CommentsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -26,6 +27,7 @@ namespace MouthfeelAPIv2.Controllers
 
         // TODO: Do we want one for "by user"? Could be displayed on Settings screen
 
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<ActionResult<IEnumerable<CommentResponse>>> GetCommentsByFood
         (
